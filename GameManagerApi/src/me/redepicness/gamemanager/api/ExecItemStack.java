16b226746f38c7312c0dba146e7b25e1a669220f
Red_Epicness <redepicness1@gmail.com>
2015-06-23 15:17:04 +0200

    Delete .iml file

--- GameManagerApi/src/me/redepicness/gamemanager/api/ExecItemStack.java
+++ GameManagerApi/src/me/redepicness/gamemanager/api/ExecItemStack.java
@@ -1,14 +1,23 @@
 package me.redepicness.gamemanager.api;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public interface ExecItemStack {
+
+    /**
+     * Called when a player clicks on the item in an inventory
+     *
+     * @param player - Player who clicked
+     */
+    void onExecute(Player player);
 
     /**
      * Checks if the itemstack passed is the origin of this ExecItemStack.
      * Compares the type of the itemstack and the display name.
      *
      * @param itemStack - ItemStack to check
+     *
      * @return - If the ItemStack is the origin
      */
     boolean isOrigin(ItemStack itemStack);
